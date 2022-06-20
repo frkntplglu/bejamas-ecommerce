96b16577e9a727997f98fea74a45cec0a5766637
Furkan Topaloğlu <furkantopaloglu@admins-MBP.home>
2022-06-20 11:18:35 +0300

    fix sorting in mobile

--- src/components/ProductListHeader/ProductListHeader.tsx
+++ src/components/ProductListHeader/ProductListHeader.tsx
@@ -23,6 +23,7 @@ const ProductListHeader = (props: any) => {
   return (
     <section className={styles.productListHeader}>
       <PageTitle mainTitle="Photography" subTitle='Premium Photos' />
+      <button onClick={() => setIsFilterModalOpen(!isFilterModalOpen)} className={styles.filterMobileButton}><Image src="/images/filter.svg" width="29" height="29" alt="Filter" /></button>
       <div className={styles.sortingWrapper}>
         <button onClick={handleSortingAlignment} className={styles.sortingBtn}><Image src="/images/sort.svg" alt="Bejamas" width="15" height="15" /></button>
         Sort By
@@ -31,7 +32,6 @@ const ProductListHeader = (props: any) => {
           <option value="alphabetical">Alphabetical</option>
         </select>
       </div>
-      <button onClick={() => setIsFilterModalOpen(!isFilterModalOpen)} className={styles.filterMobileButton}><Image src="/images/filter.svg" width="29" height="29" alt="Filter" /></button>
     </section>
   )
 }
